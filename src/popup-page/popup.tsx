de0dfe74bf310a55212ce7f06687bb3a1dc6d102
Kevin Zhou <kevinz20@vt.edu>
2022-07-30 22:53:32 -0500

    more complex control of grid

--- src/popup-page/popup.tsx
+++ src/popup-page/popup.tsx
@@ -1,24 +1,42 @@
 //run 'yarn dev' to compile and reload
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 
-const Square: FC = () => {
-    return (
-        <button className='square'>
+interface ISquareProps {
+    value: string;
+    onClick: () => void;
+}
 
-        </button>
+const Square: FC<ISquareProps> = (props: ISquareProps): JSX.Element => {
+    const { value, onClick } = props;
+
+    return (
+        <button className='square' onClick={onClick}>{value}</button>
     );
 }
 
-const Board: FC = () => {
+const Board: FC = (): JSX.Element => {
     const status: string = 'Next Player: X';
+    const [boardState, setBoardState] = useState(new Array<string>(9));
+
+    const handleClick = (i: number) => {
+        const temp = boardState.slice();
+        temp[i] = 'X';
+        setBoardState(temp);
+    };
+
     const pieces: JSX.Element[] = [];
     for (let i = 0; i < 3; i++) {
         const squares: JSX.Element[] = [];
         for (let j = 0; j < 3; j++) {
-            squares.push(<Square />)
+            const num: number = i * 3 + j;
+            squares.push(
+                <Square 
+                    value={boardState[num]} 
+                    onClick={() => handleClick(num)}
+                />);
         }
-        pieces.push(<div className='board-row'>{squares}</div>)
+        pieces.push(<div className='board-row'>{squares}</div>);
     }
     return (
         <>
@@ -28,7 +46,7 @@ const Board: FC = () => {
     );
 }
 
-export const Popup: FC = () => {
+export const Popup: FC = (): JSX.Element => {
     return (
         <div className='game'>
             <div className='game-board'>
